d0b85da4629bf22bb71c242f079e69f754a4949f
Ekmal Royhan <ekmalroyhan1010@gmail.com>
2025-04-30 14:04:00 +0700

    Footer fix typo

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="pt-20 pb-5 bg-gray-50">
-      <p className="text-center text-gray-500">Made and Authorize by Terrae</p>
+      <p className="text-center text-gray-500">
+        Made and authorized by Terrae.
+      </p>
       <p className="text-center text-gray-500">
         &copy; Lumajang Expedition Tour {new Date().getFullYear()}
       </p>
